deed6e3c6ee4a01bbf760292213efd25f88c35c3
J370 <jetong2001@gmail.com>
2019-07-13 01:33:39 +0800

    Added Past Meals and About

---
+++ app/app/src/main/java/com/treble/appetite/AboutFragment.kt
@@ -0,0 +1,14 @@
+package com.treble.appetite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.about.*
+
+class AboutFragment : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
