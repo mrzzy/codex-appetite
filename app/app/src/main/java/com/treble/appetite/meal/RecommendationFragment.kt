7565a0081bdf084e7d9e6586e3ed32dda7f44bc5
Joel Tio <joeltiojinhon@gmail.com>
2019-07-13 05:20:59 +0800

    Rename AboutFragment to RecommendationFragment

--- app/app/src/main/java/com/treble/appetite/meal/RecommendationFragment.kt
+++ app/app/src/main/java/com/treble/appetite/meal/RecommendationFragment.kt
@@ -1,14 +1,14 @@
-package com.treble.appetite
+package com.treble.appetite.meal
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.about.*
+import com.treble.appetite.R
 
-class AboutFragment : Fragment(){
+class RecommendationFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_recommendation, container, false)
     }
 }
