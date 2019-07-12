15395e9cfd65ecfd8607651deab96e2ed4e7bed0
Joel Tio <joeltiojinhon@gmail.com>
2019-07-13 04:49:01 +0800

    Change CameraFragment to CameraActivity

--- app/app/src/main/java/com/treble/appetite/camera/CameraActivity.kt
+++ app/app/src/main/java/com/treble/appetite/camera/CameraActivity.kt
@@ -1,57 +1,44 @@
 package com.treble.appetite.camera
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
 import com.treble.appetite.R
-import kotlinx.android.synthetic.main.fragment_camera.*
+import kotlinx.android.synthetic.main.activity_camera.*
 import java.io.File
 
+private const val REQUEST_CODE_PERMISSIONS = 10
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
-class CameraFragment : Fragment() {
-    /**
-     * Process result from permission request dialog box, has the request
-     * been granted? If yes, start Camera. Otherwise display a toast
-     */
+class CameraActivity : AppCompatActivity() {
+
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
                 view_finder.post { startCamera() }
             } else {
-                Toast.makeText(context,
+                Toast.makeText(baseContext,
                     "Permissions not granted by the user.",
                     Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    /**
-     * Check if all permission specified in the manifest have been granted
-     */
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            context!!, it) == PackageManager.PERMISSION_GRANTED
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun startCamera() {
-        // Create configuration object for the viewfinder use case
-        val previewConfig = PreviewConfig.Builder().apply {
-        }.build()
-
-        // Build the viewfinder use case
+        val previewConfig = PreviewConfig.Builder().build()
         val preview = Preview(previewConfig)
 
-        // Every time the viewfinder is updated, recompute layout
         preview.setOnPreviewOutputUpdateListener {
 
             // To update the SurfaceTexture, we have to remove it and re-add it
@@ -62,14 +49,13 @@ class CameraFragment : Fragment() {
             view_finder.surfaceTexture = it.surfaceTexture
         }
 
-        // Create configuration object for the image capture use case
         val imageCaptureConfig = ImageCaptureConfig.Builder()
             .setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
             .build()
-
         val imageCapture = ImageCapture(imageCaptureConfig)
+
         imagebutton_take_picture.setOnClickListener {
-            val file = File(context!!.externalMediaDirs.first(),
+            val file = File(baseContext.externalMediaDirs.first(),
                 "${System.currentTimeMillis()}.jpg")
 
             imageCapture.takePicture(file,
@@ -77,13 +63,13 @@ class CameraFragment : Fragment() {
                     override fun onError(error: ImageCapture.UseCaseError,
                                          message: String, exc: Throwable?) {
                         val msg = "Photo capture failed: $message"
-                        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
                         exc?.printStackTrace()
                     }
 
                     override fun onImageSaved(file: File) {
                         val msg = "Photo capture succeeded: ${file.absolutePath}"
-                        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
                     }
                 })
         }
@@ -91,12 +77,10 @@ class CameraFragment : Fragment() {
         CameraX.bindToLifecycle(this, preview, imageCapture)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_camera, container, false)
-    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
         // Request camera permissions
         if (allPermissionsGranted()) {
             view_finder.post { startCamera() }
